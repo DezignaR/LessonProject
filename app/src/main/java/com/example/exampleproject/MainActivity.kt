4c48782d1a4a6f9b3d0778a9d3d65171e701453e
DezignaR <Torisonra@ya.ru>
2020-10-24 18:08:09 +0900

    тестовые данные занес в список, создал отдельный класс Order, вынес функции из onCreate

--- app/src/main/java/com/example/exampleproject/MainActivity.kt
+++ app/src/main/java/com/example/exampleproject/MainActivity.kt
@@ -3,71 +3,59 @@ package com.example.exampleproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val testTimeOrder: String = "21 октября, 12:00"
-        val testSumOrder = 450.0
-        val testCountGoods = 8
-        val testStreet: String = "Василенко"
-        val testHomeNumber: String = "21/1"
-        val testEntrance: String = "4"
-        val testFloor: String = "4"
-        val testApartment: String = "112"
-        val testTimeCompleted: Int = 60
-
-        data class Order(
-            val numberOrder: Int,
-            val timeOrder: Any,
-            val sumOrder: Double,
-            val countGoods: Int,
-            val street: String,
-            val homeNumber: String,
-            val entrance: String,
-            val floor: String,
-            val apartment: String,
-            val timeCompleted: Int
+        val dataList: Map<String, Any> = mapOf(
+            "testNumberOrder" to 1,
+            "testTimeOrder" to "21 октября, 12:00",
+            "testSumOrder" to 450.0,
+            "testCountGoods" to 8,
+            "testStreet" to "Василенко",
+            "testHomeNumber" to "21/1",
+            "testEntrance" to "4",
+            "testFloor" to "4",
+            "testApartment" to "112",
+            "testTimeCompleted" to 60
         )
 
-
-        fun createTestClass(): Order {
-
-            return Order(
-                1,
-                testTimeOrder,
-                testSumOrder,
-                testCountGoods,
-                testStreet,
-                testHomeNumber,
-                testEntrance,
-                testFloor,
-                testApartment,
-                testTimeCompleted
-            )
-        }
-
-        val order = createTestClass()
-
-        fun pushTestData(order: Order) {
-            numberOrder.text = getString(R.string.number_order, order.numberOrder.toString())
-            timeOrder.text = getString(R.string.time_order, order.timeOrder)
-            sumOrder.text =
-                getString(R.string.sum_order, order.countGoods.toString(), order.sumOrder)
-            timeCompleted.text = getString(R.string.time_completed, order.timeCompleted)
-            addressOrder.text = getString(
-                R.string.address_order,
-                order.street,
-                order.homeNumber,
-                order.entrance,
-                order.floor,
-                order.apartment
-            )
-
-        }
-
+        val order = createTestClass(dataList)
         pushTestData(order)
+    }
+
+    private fun createTestClass(dataList: Map<String, Any>): Order {
+        return Order(
+            dataList["testNumberOrder"] as Int,
+            dataList["testTimeOrder"] as String,
+            dataList["testSumOrder"] as Double,
+            dataList["testCountGoods"] as Int,
+            dataList["testStreet"] as String,
+            dataList["testHomeNumber"] as String,
+            dataList["testEntrance"] as String,
+            dataList["testFloor"] as String,
+            dataList["testApartment"] as String,
+            dataList["testTimeCompleted"] as Int
+        )
+    }
+
+    private fun pushTestData(order: Order) {
+        numberOrder.text = getString(R.string.number_order, order.numberOrder)
+        timeOrder.text = getString(R.string.time_order, order.timeOrder)
+        sumOrder.text =
+            getString(R.string.sum_order, order.countGoods, order.sumOrder)
+        numOrder.text = getString(R.string.number_order, order.numberOrder)
+        timeCompleted.text = getString(R.string.time_completed, order.timeCompleted)
+        addressOrder.text = getString(
+            R.string.address_order,
+            order.street,
+            order.homeNumber,
+            order.entrance,
+            order.floor,
+            order.apartment
+        )
+
     }
 }
---
+++ app/src/main/java/com/example/exampleproject/Order.kt
@@ -0,0 +1,14 @@
+package com.example.exampleproject
+
+ class Order(
+    val numberOrder: Int,
+    val timeOrder: Any,
+    val sumOrder: Double,
+    val countGoods: Int,
+    val street: String,
+    val homeNumber: String,
+    val entrance: String,
+    val floor: String,
+    val apartment: String,
+    val timeCompleted: Int
+)
